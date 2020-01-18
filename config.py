e868d4f175da61dca5ea1781cf22752f0d9fc84c
jimparr <jim.parr@mclaren.com>
2020-01-18 18:00:19 +0000

    updated base beers

--- config.py
+++ config.py
@@ -12,15 +12,13 @@ punk_df = pd.read_pickle(os.path.join(config_path, 'punk_df_201910152214.pickle'
 
 # Default beers
 available_beers = [
-    'Nanny State',
     'Punk IPA 2010 - Current',
     'Dead Pony Club',
     'Elvis Juice V2.0 - Prototype Challenge',
     'Hazy Jane',
-    'Lost Dog (w/Lost Abbey)',
-    '5am Saint',
-    'Alice Porter',
-    'Indie Pale Ale'
+    'Indie Pale Ale',
+    'Hazy Jane',
+    'Clockwork Tangerine'
 ]
 
 features = ['abv', 'ibu', 'ebc']
